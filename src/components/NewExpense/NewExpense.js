f448615ef9e6264f9a17f6ad1c70b617e2daecc6
Sarabjeet <sabysingh298@gmail.com>
2021-04-17 22:16:21 +0530

    state and Event Handling

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,18 @@
+import "./NewExpense.css";
+import NewExpenseForm from "./NewExpenseForm";
+const NewExpense = (props) => {
+    const saveExpenseDataHandler =(enteredExpenseData) =>{
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        console.log(expenseData);
+        props.onAddExpense(expenseData);
+    }
+  return (
+    <div className="new-expense">
+      <NewExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+    </div>
+  );
+};
+export default NewExpense;
