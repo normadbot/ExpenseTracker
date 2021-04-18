8e58cbca22836a7110435a5eac47ea90c9efb3b8
Sarabjeet <sabysingh298@gmail.com>
2021-04-18 11:03:35 +0530

    Conditional Statements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -24,16 +25,19 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div className="App">
-     <NewExpense onAddExpense={addExpenseHandler}/> 
-      <Expenses items={expenses}/>  
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
